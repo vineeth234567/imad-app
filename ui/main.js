2c48c6a978064b2dd12bf476f0dc9f7d4c9c10ec
vineeth234567 <30755078+vineeth234567@users.noreply.github.com>
2017-08-20 11:18:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //CREATE A REQUEST TO THE COUNTER END POINT
+    var request=new XMLhttprequest();
+    // capturing and storinng the response in a variable
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+            //TAKE SOME ACTION
+            if(request.status==200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHtml=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://kvenkatvineeth.imad.hasura-app.io/counter', true);
+    request.send(null);
+} ;
